307f329c27eee34e6a7c4c4debf93bdf9672314f
Svetlana Goncharova <Goncharova_S.A@mail.ru>
2020-07-09 19:09:30 +0300

    Выпадашка

---
+++ js/hamburger.js
@@ -0,0 +1,19 @@
+const hamburger = document.querySelector(".hamburger");
+const burger = document.querySelector(".burger");
+const close = document.querySelector(".close");
+
+hamburger.addEventListener('click', function () {
+  burger.classList.add('burger_opened');
+});
+
+burger.addEventListener('click', function (e) {
+  if (!e.target.classList.contains('burger__item') || e.target.classList.contains('close')) {
+    burger.classList.remove('burger_opened');
+  }
+  
+  if (e.target.classList.contains('burger__item')) {
+    console.log('меняем секцию');
+    burger.classList.remove('burger_opened');
+  }
+  
+})
